87bdc13d415a561f8d958a0f24c6ca3708aced97
Emile Pels <mail@emilepels.nl>
2020-03-03 18:18:30 +0100

    Add cmd/spartyd daemon

---
+++ cmd/spartyd/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/epels/sparty/handler"
+	"github.com/epels/sparty/jobqueue"
+	"github.com/epels/sparty/spotify"
+)
+
+var (
+	errLog  = log.New(os.Stderr, "[ERROR]: ", log.LstdFlags|log.Lshortfile)
+	infoLog = log.New(os.Stdout, "[INFO]: ", log.LstdFlags|log.Lshortfile)
+)
+
+func main() {
+	// PORT is set by Google App Engine.
+	p := os.Getenv("PORT")
+	if p == "" {
+		p = "8080"
+	}
+	addr := ":" + p
+	jq := jobqueue.NewMemory()
+	sc := spotify.NewClient(
+		mustGetenv("SPOTIFY_CLIENT_ID"),
+		mustGetenv("SPOTIFY_CLIENT_SECRET"),
+		mustGetenv("SPOTIFY_REFRESH_TOKEN"),
+	)
+
+	// Channels that can cancel the execution of the daemon.
+	errCh := make(chan error, 2)
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start the job consumer/worker.
+	jqCtx, jqCancel := context.WithCancel(context.Background())
+	defer jqCancel()
+	go func() {
+		infoLog.Print("Starting job worker")
+		err := jq.Consume(jqCtx, func(uri string) {
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			if err := sc.AddToQueue(ctx, uri); err != nil {
+				errLog.Printf("spotify: Client.AddToQueue: %s", err)
+			}
+			infoLog.Printf("Enqueued %s", uri)
+		})
+		errCh <- fmt.Errorf("jobqueue: memory.Consume: %s", err)
+	}()
+
+	// Create the API server and start listening.
+	h := handler.New(errLog, infoLog, jq)
+	s := http.Server{
+		Addr:    addr,
+		Handler: h,
+
+		IdleTimeout:  1 * time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	go func() {
+		infoLog.Printf("Starting server on %q", addr)
+		err := s.ListenAndServe()
+		errCh <- fmt.Errorf("net/http: Server.ListenAndServe: %s", err)
+	}()
+
+	// Handle shutdown due to API error, job consumer failure, or signal.
+	select {
+	case err := <-errCh:
+		errLog.Printf("Exiting with error: %s", err)
+	case sig := <-sigCh:
+		infoLog.Printf("Exiting with signal: %s", sig)
+	}
+
+	sCtx, sCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer sCancel()
+	if err := s.Shutdown(sCtx); err != nil {
+		errLog.Printf("net/http: Server.Shutdown: %v", err)
+	}
+	if err := jq.Close(); err != nil {
+		errLog.Printf("jobqueue: memory.Close: %s", err)
+	}
+}
+
+func mustGetenv(key string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		errLog.Fatalf("Missing required environment variable: %s", key)
+	}
+	return val
+}
